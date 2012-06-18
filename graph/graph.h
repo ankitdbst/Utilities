3291b2c16e181236b032570eff3783642607eb74
Ankit Gupta <mailtoankitgupta@gmail.com>
2012-06-18 21:59:42 +0530

    Implement graph algorithms.

---
+++ graph/graph.h
@@ -0,0 +1,38 @@
+/**
+ * Class to implement various graph routines
+ */
+#ifndef GRAPH_H
+#define GRAPH_H
+
+#include <iostream>
+#include <cstdio>
+#include <cstdlib>
+#include <string.h>
+#include <vector>
+#include <string>
+#include <cmath>
+#include <map>
+#include <iomanip>
+#include <algorithm>
+using namespace std;
+
+typedef vector<int> VI;
+typedef vector< VI > VVI;
+
+#define debug(args...) dbg(),args
+#define FOR(A,B,C) for(int A=B;A<C;++A)
+#define RFOR(A,B,C) for(int A=B;A>=C;--A)
+#define PB(A,B) A.push_back(B);
+
+class Graph 
+{
+    public:
+        VVI trans_closure(VVI adj, int n);
+        VVI floyd_warshall(VVI adj, int n);
+
+    private:
+        VVI prod(VVI m1, VVI m2, int n);
+
+};
+
+#endif // GRAPH_H
